09ece16016e20a8b715453d42f8a52ecc5cdd602
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2025-02-15 10:22:22 +0000

    Add initial ProtocolCode enum

--- src/MyCobot.jl
+++ src/MyCobot.jl
@@ -4,4 +4,19 @@ using LibSerialPort
 
 greet() = print("Hello World!")
 
+@enum ProtocolCode begin
+    # BASIC
+    HEADER = 0xFE
+    FOOTER = 0xFA
+
+    # MDI MODE AND OPERATION
+    GET_ANGLES = 0x20
+
+    # ATOM IO
+    GET_DIGITAL_INPUT = 0x62
+
+    # Basic
+    GET_BASIC_INPUT = 0xA1
+end
+
 end # module MyCobot
